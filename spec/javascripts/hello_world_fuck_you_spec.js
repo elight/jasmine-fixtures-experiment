069746891804a12725f5fbe13cb7379220e6cf86
Evan Light <evan.light@tripledogdare.net>
2011-06-29 21:12:02 -0700

    Works in the browser
    DOESN'T work on the command line

--- spec/javascripts/hello_world_fuck_you_spec.js
+++ spec/javascripts/hello_world_fuck_you_spec.js
@@ -2,9 +2,10 @@
 
 describe('hello_world', function() {
   it("should tell us 'fuck you' when you click the h1", function() {
-    template("hello_world.jasmine_fixture");
-    expect($('#test h1').text()).toNotContain("Fuck You");
-    //$("h1").click();
-//    expect($('#test h1').text()).toContain("Fuck You");
+    spec.loadFixture("hello_world");
+    expect($('h1').text()).toNotContain("Fuck you");
+    $("h1").click();
+    console.log($("h1").text());
+    expect($('h1').text()).toContain("Fuck you");
   });
 });
